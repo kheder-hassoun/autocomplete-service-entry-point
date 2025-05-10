d2bec053e23e92176ffb736fd61f96e76791b69b
kheder-hassoun <khdrhswn32@gmail.com>
2025-05-10 14:01:40 +0300

    Initial Commit, from spring boot initializer

---
+++ src/main/java/me/autocomplete_api/AutocompleteApiApplication.java
@@ -0,0 +1,13 @@
+package me.autocomplete_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/autocomplete_api/AutocompleteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.autocomplete_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
