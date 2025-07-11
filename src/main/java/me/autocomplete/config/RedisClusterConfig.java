af1e1d1cd948730599a01889891b3f080a90a767
kheder-hassoun <khdrhswn32@gmail.com>
2025-07-11 12:31:53 +0300

    update for k8s deployment

--- src/main/java/me/autocomplete/config/RedisClusterConfig.java
+++ src/main/java/me/autocomplete/config/RedisClusterConfig.java
@@ -1,22 +1,31 @@
 package me.autocomplete.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 
-import java.util.List;
+import java.util.Arrays;
 
 @Configuration
+@EnableConfigurationProperties
 public class RedisClusterConfig {
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(
-                List.of("redis-node-1:7000", "redis-node-2:7001", "redis-node-3:7002")
-        );
-        return new LettuceConnectionFactory(clusterConfig);
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.cluster.nodes}") String nodes,
+            @Value("${spring.data.redis.password:}")     String password) {
+
+        RedisClusterConfiguration cfg =
+                new RedisClusterConfiguration(Arrays.asList(nodes.split("\\s*,\\s*")));
+        if (StringUtils.hasText(password)) {
+            cfg.setPassword(password);
+        }
+        return new LettuceConnectionFactory(cfg);
     }
 
     @Bean
